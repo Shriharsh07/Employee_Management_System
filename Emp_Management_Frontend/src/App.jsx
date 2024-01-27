f896f2e68f7b68564e85791091740abcaca14ff8
Shriharsh07 <shriharshdpattar@gmail.com>
2024-01-27 19:27:27 +0530

    Add Employee

--- Emp_Management_Frontend/src/App.jsx
+++ Emp_Management_Frontend/src/App.jsx
@@ -2,14 +2,25 @@ import './App.css'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import ListEmployee from './components/ListEmployee'
+import Employee from './components/Employee'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-      <Header />
-      <ListEmployee />
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          {/* http://localhost:3000 */}
+          <Route path='/' element={<ListEmployee />}></Route>
+          {/* http://localhost:3000/employees */}
+          <Route path='/employees' element={<ListEmployee />}></Route>
+          {/* http://localhost:3000/add-employee */}
+          <Route path='/add-employee' element={<Employee />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   )
 }
